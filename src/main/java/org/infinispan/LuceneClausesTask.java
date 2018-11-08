674cb49d0a786708049de080c7df52544b376944
Gustavo Fernandes <gustavonalle@gmail.com>
2018-11-08 19:27:08 +0000

    Add parametrisation

--- src/main/java/org/infinispan/LuceneClausesTask.java
+++ src/main/java/org/infinispan/LuceneClausesTask.java
@@ -1,13 +1,19 @@
 package org.infinispan;
 
+import java.util.Map;
+import java.util.Optional;
+
 import org.apache.lucene.search.BooleanQuery;
 import org.infinispan.tasks.ServerTask;
 import org.infinispan.tasks.TaskContext;
 
 public class LuceneClausesTask implements ServerTask<Integer> {
 
+   private TaskContext taskContext;
+
    @Override
    public void setTaskContext(TaskContext taskContext) {
+      this.taskContext = taskContext;
    }
 
    @Override
@@ -17,7 +23,11 @@ public class LuceneClausesTask implements ServerTask<Integer> {
 
    @Override
    public Integer call() {
-      BooleanQuery.setMaxClauseCount(2000);
+      Optional<Map<String, ?>> parameters = taskContext.getParameters();
+      if (parameters.isPresent()) {
+         Integer maxClauses = (Integer) parameters.get().get("maxClauses");
+         BooleanQuery.setMaxClauseCount(maxClauses);
+      }
       return BooleanQuery.getMaxClauseCount();
    }
 }
